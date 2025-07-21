df3d21f1091414215d309db0276f077aa22ca9d5
Kento.Yamada <ymd65536@outlook.com>
2025-07-21 06:08:58 +0000

    Add pip install command for strands-agents in postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,5 +2,6 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y python3 python3-pip
+pip install --user --break-system-packages strands-agents
 pip config set install.user 'false'
 pip config set global.index-url https://pypi.org/simple
